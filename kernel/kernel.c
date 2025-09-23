01c90468cade80564f9a5eb4f6e5cbb381a7bf8e
Kenzy YACINI <kyacini@f2r2s5.clusters.42paris.fr>
2025-09-23 13:01:39 +0200

    last correction

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,26 @@
+#include <stdint.h>
+
+#define VGA_MEM ((volatile uint16_t*)0xB8000)
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+static uint8_t vga_color = 0x07;
+
+void clear_screen() {
+    for (int y = 0; y < VGA_HEIGHT; y++)
+        for (int x = 0; x < VGA_WIDTH; x++)
+            VGA_MEM[y * VGA_WIDTH + x] = ((uint16_t)vga_color << 8) | ' ';
+}
+
+void print(const char *s) {
+    int i = 0;
+    while (s[i]) {
+        VGA_MEM[i] = ((uint16_t)vga_color << 8) | (uint8_t)s[i];
+        i++;
+    }
+}
+
 void kernel_main(void) {
-    char *video_memory = (char *)0xb8000;
-    video_memory[0] = '4';
-    video_memory[1] = 0x09;
-    video_memory[2] = '2';
-    video_memory[3] = 0x09;
+    clear_screen();
+    print("42");
+    for(;;) __asm__ __volatile__("hlt");
 }
