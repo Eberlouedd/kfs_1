fb0a16ece23d81399682acda640531032aa94c6d
Kenzy YACINI <kyacini@f6r13s14.clusters.42paris.fr>
2025-09-24 14:17:05 +0200

    final

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,13 +1,23 @@
+bits 32
 section .multiboot
 align 4
     dd 0x1BADB002
     dd 0x00
     dd -(0x1BADB002 + 0x00)
 
+section .bss
+align 16
+stack_bottom:
+    resb 16384
+stack_top:
+
 section .text
 global _start
 _start:
+    mov esp, stack_top
     extern kernel_main
     call kernel_main
 .hang:
+    cli
+    hlt
     jmp .hang
